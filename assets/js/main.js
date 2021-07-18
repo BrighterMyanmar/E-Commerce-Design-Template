e9f95230f20cddde78275738df2dada2513af6a9
Brighter Myanmar <brightermyanmar@gmail.com>
2021-07-18 16:03:34 +0630

    ECommerce Site

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,18 @@ for(let i = 0; i < lis.length; i++){
         lis[i].classList.add('active');
     })
 }
+
+let i = 0;
+let mLeft = 0;
+let slides = document.querySelector('.slides');
+var myInterval = setInterval(()=>{
+    mLeft += 700;
+    i++;
+    if(i <= 4){
+        slides.style.marginLeft = -mLeft + "px";
+    }else{
+        i = 0;
+        mLeft = 0;
+        slides.style.marginLeft = mLeft + "px";
+    }
+},5000)
